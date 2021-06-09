18bfce02a3d10119d5b665c39a33b28ba60e229b
agustingalaxie <71106735+agustingalaxie@users.noreply.github.com>
2021-06-09 17:19:40 -0300

    displaypool done
    display pool now displays the selected dices and their type

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,36 +3,40 @@ import DisplayPool from "./DisplayPool.js";
 
 const Form = ({ pool, setPool }) => {
     const setPoolHandlerNormal = (e) => {
+        const normalDicesPool = pool.filter((dice) => dice.type === "normal");
+        const beastDicesPool = pool.filter((dice) => dice.type === "beast");
         let value = e.target.value;
-        let length = pool.length;
+        let length = normalDicesPool.length;
         if (value > length) {
-            console.log(value)
-            setPool([...pool, { value: {value}, type: "normal", id: Math.random() * 100 }]);
-        } else if(value <= length){
-            console.log(value)
-            setPool(pool.slice(0, value));
+            setPool([...pool, { type: "normal", id: Math.random() * 100 }]);
+        } else if (value < length) {  //<--- ?????????????????????????????????????????????????????
+            normalDicesPool.pop();
+            const newPool = beastDicesPool.concat(normalDicesPool);
+            setPool(newPool);
         }
-    };
+    }
     const setPoolHandlerBeast = (e) => {
+        const normalDicesPool = pool.filter((dice) => dice.type === "normal");
+        const beastDicesPool = pool.filter((dice) => dice.type === "beast");
         let value = e.target.value;
-        let length = pool.length;
+        let length = beastDicesPool.length;
         if (value > length) {
-            console.log(value)
-            setPool([...pool, { value: {value}, type: "beast", id: Math.random() * 100 }]);
-        } else if(value <= length){
-            console.log(value)
-            setPool(pool.slice(0, value));
-        }
-    };
-    return (
-        <div>
-            <h3>Pool de dados NORMALES</h3>
-            <input type="number" onChange={setPoolHandlerNormal} min="1" max="10" placeholder="1" />
-            <h3>Pool de dados DE BESTIA</h3>
-            <input type="number" onChange={setPoolHandlerBeast} min="1" max="10" placeholder="1" />
-            <DisplayPool pool={pool} />
-        </div>
-    )
-}
+            setPool([...pool, { type: "beast", id: Math.random() * 100 }]);
+        } else if (value < length) {
+            beastDicesPool.pop();
+            const newPool = beastDicesPool.concat(normalDicesPool);
+            setPool(newPool);
+        };
+    }
+        return (
+            <div>
+                <h3>Pool de dados normales</h3>
+                <input type="number" onChange={setPoolHandlerNormal} min="1" max="10" placeholder="0" />
+                <h3>Dados de Bestia</h3>
+                <input type="number" onChange={setPoolHandlerBeast} min="0" max="10" placeholder="0" />
+                <DisplayPool pool={pool} />
+            </div>
+        )
+    }
 
-export default Form;
+    export default Form;
